99a1253fd12e735ee3bce493e1fd98d17d2be25d
phenpessoa <phenpessoa@gmail.com>
2024-05-31 18:40:32 -0300

    basic settings on main

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def main() -> None:
-    print()
+import streamlit as st
+import os
+
+def main():
+    st.set_page_config(page_title="CITAL", page_icon="🚀", layout="wide")
+
+    json_path = os.path.join(os.getcwd(), "data", "cital_data.json")
+    if not os.path.exists(json_path):
+        raise FileNotFoundError(f"Arquivo não encontrado em {json_path}")
 
 if __name__ == "__main__":
     main()
