cc8d8a78ab211a548973893cd25d0171f621bd2a
phenpessoa <phenpessoa@gmail.com>
2024-05-31 18:40:49 -0300

    basic model implementation

---
+++ model/model.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import json
+
+class ModeloCital:
+    def __init__(self, json_path: str):
+        with open(json_path) as f:
+            data = json.load(f)
+
+        try:
+            self.df = pd.json_normalize(data, record_path=['itens'], meta=['numero_n_fe', 'data', 'forma_pagamento'])
+        except pd.errors.ParserError:
+            raise ValueError(f"Erro ao processar o arquivo json")
+
+        self.df['data'] = pd.to_datetime(self.df['data'], utc=True)
